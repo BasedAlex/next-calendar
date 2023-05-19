9eecc1855bea1b7234c35609dac634bf4c43b5fd
Liam <backgroundpositioncenter@gmail.com>
2023-05-20 00:49:49 +0300

    redesign sidebar's layout

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,90 +1,58 @@
-'use client'
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
-	AppstoreOutlined,
-	MailOutlined,
-	SettingOutlined,
-} from '@ant-design/icons'
-import type { MenuProps, MenuTheme } from 'antd'
-import { Menu, Switch } from 'antd'
+	DesktopOutlined,
+	FileOutlined,
+	PieChartOutlined,
+	TeamOutlined,
+	UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
-type MenuItem = Required<MenuProps>['items'][number]
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
 
 function getItem(
 	label: React.ReactNode,
-	key?: React.Key | null,
+	key: React.Key,
 	icon?: React.ReactNode,
 	children?: MenuItem[],
-	type?: 'group'
 ): MenuItem {
 	return {
 		key,
 		icon,
 		children,
 		label,
-		type,
-	} as MenuItem
+	} as MenuItem;
 }
 
 const items: MenuItem[] = [
-	getItem('Navigation One', 'sub1', <MailOutlined />, [
-		getItem('Option 1', '1'),
-		getItem('Option 2', '2'),
-		getItem('Option 3', '3'),
-		getItem('Option 4', '4'),
+	getItem('Option 1', '1', <PieChartOutlined />),
+	getItem('Option 2', '2', <DesktopOutlined />),
+	getItem('User', 'sub1', <UserOutlined />, [
+		getItem('Tom', '3'),
+		getItem('Bill', '4'),
+		getItem('Alex', '5'),
 	]),
+	getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+	getItem('Files', '9', <FileOutlined />),
+];
 
-	getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-		getItem('Option 5', '5'),
-		getItem('Option 6', '6'),
-		getItem('Submenu', 'sub3', null, [
-			getItem('Option 7', '7'),
-			getItem('Option 8', '8'),
-		]),
-	]),
-
-	getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-		getItem('Option 9', '9'),
-		getItem('Option 10', '10'),
-		getItem('Option 11', '11'),
-		getItem('Option 12', '12'),
-	]),
-]
-
-const Sidebar: React.FC = () => {
-	const [theme, setTheme] = useState<MenuTheme>('light')
-	const [current, setCurrent] = useState('1')
-
-	const changeTheme = (value: boolean) => {
-		setTheme(value ? 'dark' : 'light')
-	}
-
-	const onClick: MenuProps['onClick'] = e => {
-		console.log('click ', e)
-		setCurrent(e.key)
-	}
+const App: React.FC = () => {
+	const [collapsed, setCollapsed] = useState(true);
+	const {
+		token: { colorBgContainer },
+	} = theme.useToken();
 
 	return (
-		<>
-			<Switch
-				checked={theme === 'dark'}
-				onChange={changeTheme}
-				checkedChildren='Dark'
-				unCheckedChildren='Light'
-			/>
-			<br />
-			<br />
-			<Menu
-				theme={theme}
-				onClick={onClick}
-				style={{ width: 300 }}
-				defaultOpenKeys={['sub1']}
-				selectedKeys={[current]}
-				mode='inline'
-				items={items}
-			/>
-		</>
-	)
-}
+		<Layout style={{ minHeight: '100vh' }}>
+			<Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+				<div className="demo-logo-vertical p-1" />
+				<Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+			</Sider>
+		</Layout>
+	);
+};
 
-export default Sidebar
+export default App;
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,25 +4,27 @@ import CalendarNew from '@/components/components/Calendar/Calendar'
 import MiniCalendar from '@/components/components/MiniCalendar/MiniCalendar'
 import Navbar from '@/components/components/Navbar/Navbar'
 import Sidebar from '@/components/components/Sidebar/Sidebar'
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 
 const Main = () => {
-	const [showSidebar, setShowSidebar] = useState(true)
+    const [showSidebar, setShowSidebar] = useState(true)
 
-	return (
-		<>
-			<Navbar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-			<main className='flex'>
-				{showSidebar && (
-					<div>
-						<Sidebar />
-						<MiniCalendar />
-					</div>
-				)}
-				<CalendarNew />
-			</main>
-		</>
-	)
+    return (
+        <>
+            <main className='flex'>
+                {showSidebar && (
+                    <div>
+                        <Sidebar/>
+                        {/*<MiniCalendar />*/}
+                    </div>
+                )}
+                <div>
+                    <Navbar showSidebar={showSidebar} setShowSidebar={setShowSidebar}/>
+                    <CalendarNew/>
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default Main
